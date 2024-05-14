e3029d7bdc2a0eaf7600748a9d026c8f4f74a16e
Pratham Shah <54094684+Pratham-Shah@users.noreply.github.com>
2024-05-13 23:17:25 -0400

    Update settings.py

--- pds_project2/settings.py
+++ pds_project2/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'project',
         'USER': 'postgres',
-        'PASSWORD': 'Prasha@2001',
+        'PASSWORD': 'pds123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
